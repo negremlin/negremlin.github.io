2b95a403ec63cc087b22934d9131b9cc1b437c86
negremlin <relforsel@gmail.com>
2025-09-03 15:49:49 +0300

    Delete _ipsx01 directory

--- _ipsx01/@.js
+++
@@ -1,9 +0,0 @@
-const meta = document.getElementsByTagName("meta");
-
-console.log(document.getElementsByTagName("html")[0].innerHTML);
-
-for (const m of meta) {
-    if (m.name === "-title") {
-        document.title = m.content;
-    }
-}
